c859c15f5aa4cb00ba70e6284fdb3ad54652265c
BAHOHCEHC <bahohcehc@gmail.com>
2020-01-26 02:52:37 +0200

    backend: add passport strategies, login, register

--- fullstack/config/keys.js
+++ fullstack/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   // mongoURI: 'mongodb+srv://ivan:123456z@cluster0-dmk4e.mongodb.net/test?retryWrites=false&w=majority',
   mongoURI: 'mongodb+srv://admin:admin@fullstackcluster-sjg3z.mongodb.net/test?retryWrites=true&w=majority',
-  jwt: 'dev-jwt'
+  jwt: 'dev-jwt',
 };
