159003ad3f80455d8148f4270ceacc744a3e0f3b
BAHOHCEHC <bahohcehc@gmail.com>
2020-01-26 07:33:36 +0200

    backend: done all functional controllers

--- fullstack/routes/analytic.js
+++ fullstack/routes/analytic.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const passport = require('passport');
+
 const controller = require('../controllers/analytic');
 const router = express.Router();
 
 //localhost:6000/api/auth/login
-router.get('/overview', controller.overview);
+router.get('/overview', passport.authenticate('jwt', { session: false }), controller.overview);
 
-router.get('/analitic', controller.analytic);
+router.get('/analitic', passport.authenticate('jwt', { session: false }), controller.analytic);
 module.exports = router;
