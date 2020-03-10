cb0d7caf375cc2e53a387a028ca7da3b88fbfc98
BAHOHCEHC <bahohcehc@gmail.com>
2020-03-10 04:46:56 +0200

    production preparing

--- fullstack/config/keys.js
+++ fullstack/config/keys.js
@@ -1,5 +1,10 @@
-module.exports = {
-  // mongoURI: 'mongodb+srv://ivan:123456z@cluster0-dmk4e.mongodb.net/test?retryWrites=false&w=majority',
-  mongoURI: 'mongodb+srv://admin:admin@fullstackcluster-sjg3z.mongodb.net/test?&w=majority',
-  jwt: 'dev-jwt',
-};
+// module.exports = {
+//   // mongoURI: 'mongodb+srv://ivan:123456z@cluster0-dmk4e.mongodb.net/test?retryWrites=false&w=majority',
+//   mongoURI: 'mongodb+srv://admin:admin@fullstackcluster-sjg3z.mongodb.net/test?&w=majority',
+//   jwt: 'dev-jwt',
+// };
+if (procces.env.NODE_ENV === 'production') {
+	module.exports = require('./keys.prod');
+} else {
+	module.exports = require('./keys.dev');
+}
