f2807a347f796d0dc6ba01dca92b2345f2bebbd1
BAHOHCEHC <bahohcehc@gmail.com>
2020-03-10 11:19:51 +0200

    make it better

--- fullstack/app.js
+++ fullstack/app.js
@@ -41,7 +41,7 @@ app.use('/api/position', positionRoutes);
 if (procces.env.NODE_ENV === 'production') {
 	app.use(express.static('client/dist/client'));
 
-	app.get('*', (req, res) => {
+	app.get('*', function(req, res) {
 		// res.sendFile(path.join(__dirname + 'client/dist/client/index.html'));
 		res.sendFile(path.resolve(__dirname, 'client', 'dist', 'client', 'index.html'));
 	});
