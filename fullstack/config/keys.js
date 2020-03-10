014427de75eb49e4584ccc97f469bd2a04b2acdb
BAHOHCEHC <bahohcehc@gmail.com>
2020-03-10 05:56:02 +0200

    updates to deploy to heroku

--- fullstack/config/keys.js
+++ fullstack/config/keys.js
@@ -1,9 +1,4 @@
-// module.exports = {
-//   // mongoURI: 'mongodb+srv://ivan:123456z@cluster0-dmk4e.mongodb.net/test?retryWrites=false&w=majority',
-//   mongoURI: 'mongodb+srv://admin:admin@fullstackcluster-sjg3z.mongodb.net/test?&w=majority',
-//   jwt: 'dev-jwt',
-// };
-if (procces.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
 	module.exports = require('./keys.prod');
 } else {
 	module.exports = require('./keys.dev');
